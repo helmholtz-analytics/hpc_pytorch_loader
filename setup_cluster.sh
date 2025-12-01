59b96b62c64f30a6f0b6eec5b02b0e3158b578e7
hmouda1 <hmouda1@jrlogin04.jureca>
2025-12-01 17:49:00 +0100

    update setup scripts for cluster installation and local installation

--- setup_cluster.sh
+++ setup_cluster.sh
@@ -8,14 +8,7 @@ ml Python/3.12.3
 
 python -m venv dataloadenv
 source dataloadenv/bin/activate
-pip install --upgrade pip
-pip install torch
-pip install torchvision
-pip install tqdm
-pip install requests
-pip install pyarrow
-pip install h5py
-pip install pillow
-pip install nvidia-ml-py3
-pip install pandas
+
+pip install -e .
+
 
--- setup_local.sh
+++ setup_local.sh
@@ -1,15 +1,8 @@
 python3 -m venv dataloadenv
+
 source dataloadenv/bin/activate
-pip install --upgrade pip
-pip install torch
-pip install torchvision
-pip install tqdm
-pip install requests
-pip install pyarrow
-pip install h5py
-pip install pillow
-pip install nvidia-ml-py3
-pip install pandas
+
+pip install -e .
 
 export CUDA_HOME=/usr/local/cuda
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$CUDA_HOME/lib64
